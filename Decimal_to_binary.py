d00d4643e937ea1c39a3d20188b3438b1f90fe03
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2025-02-28 23:41:34 +0530

    Create Decimal_to_binary.py

---
+++ Decimal_to_binary.py
@@ -0,0 +1,30 @@
+# 10 in binary = 1010
+# Then we toggle this binary 1010 = 0101
+# Then convert this toggeled binary to decimal 0101 = 5
+
+# Here 10 is the input and 5 is the output
+
+
+# Taking Input
+
+n = int(input())
+
+# Converting decimal to binary
+
+def decimal_to_binary(n):
+    if n == 0:
+        return "0"
+    binary = ""
+
+    while n > 0:
+        reminder = n%2
+        binary = str(reminder) + binary
+        n = n//2
+
+    return binary
+# print(decimal_to_binary(n))
+b = decimal_to_binary(n)
+
+
+
+
